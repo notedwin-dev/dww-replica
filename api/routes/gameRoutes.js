de0c271e8826e497b685931ae1a2683c24cb1606
notedwin-dev <notedwin.music@gmail.com>
2025-05-29 15:50:15 +0800

    refactor: remove unused crypto utility import from gameRoutes

--- api/routes/gameRoutes.js
+++ api/routes/gameRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const supabase = require('../../config/supabaseClient');
-const jwt = require('jsonwebtoken');
-const cryptoUtils = require('../../utils/crypto');
+const jwt = require("jsonwebtoken");
 
 // Middleware to verify JWT tokens
 const authenticateToken = (req, res, next) => {
